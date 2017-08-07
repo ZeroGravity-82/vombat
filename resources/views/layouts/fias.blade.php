36aae426ff3e444e4b9b9c31abd0c7b6e9ace5a8
Рябков Николай <ZeroGravity.82@gmail.com>
2017-08-07 08:43:38 +0700

    wip

---
+++ resources/views/layouts/fias.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="card">
+                <div class="card-block">
+                    <div class="card-title">Центр обновления адресов Вомбат</div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
