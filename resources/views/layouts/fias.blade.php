5bc9be5705e4c77838538b9934c6179c1ae34768
ZeroGravity-82 <ZeroGravity.82@gmail.com>
2017-08-07 18:40:24 +0700

    wip

--- resources/views/layouts/fias.blade.php
+++ resources/views/layouts/fias.blade.php
@@ -4,8 +4,24 @@
     <div class="row">
         <div class="col-xs-12">
             <div class="card">
+                <h1 class="card-header">Центр обновления адресов Вомбат</h1>
                 <div class="card-block">
-                    <div class="card-title">Центр обновления адресов Вомбат</div>
+                    <h2 class="card-title">Состояние обновления</h2>
+                    <p class="'card-text">Всегда следите за обновлениями.</p>
+                    <a href="/" class="btn btn-primary">На главную</a>
+                </div>
+                <div class="card-footer">
+                    <p>Последний поиск обновлений:</p>
+                    <p>Обновления устанавливались:</p>
+                    <a hreh="#" class="card-link">История обновлений</a>
+
+                    <p class="text-muted">Обновления будут загружены и установлены автоматически.</p>
+                    <!--
+                    <p class="text-muted">Обновления будут загружены автоматически, установка производится вручную.</p>
+                    <p class="text-muted">Обновления будут найдены автоматически, загрузка и установка производятся вручную.</p>
+                    <p class="text-muted">Проверка наличия обновлений отключена.</p>
+                    -->
+                    <a hreh="#" class="card-link">Настройка параметров</a>
                 </div>
             </div>
         </div>
